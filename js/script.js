53da48cd289617c9e1c99fbf621c5a0ed4f8e66d
arungicahaya <akuarung83@gmail.com>
2024-01-07 21:46:56 +0700

    add local storage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const todos = []
 const RENDER_EVENT = 'render-todo';
+const STORAGE_KEY = 'todos-data'
+const SAVED_EVENT = ''
 
 function generateId(){
 	return +new Date()
@@ -24,6 +26,26 @@ function findTodoIndex(todoId) {
   return -1;
 }
 
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {	
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+function saveData(){
+	if(isStorageExist()){
+		const parsed = JSON.stringify(todos)
+		localStorage.setItem(STORAGE_KEY, parsed)
+		document.dispatchEvent(new Event(SAVED_EVENT))
+	}
+}
+
 function addTaskToCompleted(todoId) {
 	const todoTarget = findTodo(todoId)
 
@@ -31,6 +53,7 @@ function addTaskToCompleted(todoId) {
 
 	todoTarget.isCompleted = true;
 	document.dispatchEvent(new Event(RENDER_EVENT));
+	saveData()
 }
 
 function removeTaskFromCompleted(todoId) {
@@ -40,6 +63,7 @@ function removeTaskFromCompleted(todoId) {
  
   todos.splice(todoTarget, 1);
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData()
 }
 
 function undoTaskFromCompleted(todoId){
@@ -49,7 +73,24 @@ function undoTaskFromCompleted(todoId){
 
 	todoTarget.isCompleted = false;
 	document.dispatchEvent(new Event(RENDER_EVENT))
+	saveData()
 }
+
+function loadDataFromStorage(){
+	const serializedData = localStorage.getItem(STORAGE_KEY)
+	let data = JSON.parse(serializedData);
+
+
+	if(data !== null){
+		for(const todo of data){
+			todos.push(todo)
+		}
+	}
+
+	document.dispatchEvent(new Event(RENDER_EVENT))
+}
+
+
 
 
 function makeTodo(todoObject){
@@ -143,6 +184,7 @@ function addTodo(){
 	todos.push(todoObject)
 
 	document.dispatchEvent(new Event(RENDER_EVENT))
+	saveData()
 }
 
 
@@ -152,4 +194,10 @@ document.addEventListener('DOMContentLoaded', function(){
 		event.preventDefault()
 		addTodo()
 	})
+
+	console.log(isStorageExist(), 'checkstorage')
+
+	if(isStorageExist()){
+		loadDataFromStorage()
+	}
 })
